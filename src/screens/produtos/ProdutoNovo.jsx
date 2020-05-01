ee8144f7b86da448192fefaf54171aee4481aa59
eclesio <eclesiomelo.1@gmail.com>
2020-05-01 15:35:38 -0400

    products table and image upload

--- src/screens/produtos/ProdutoNovo.jsx
+++ src/screens/produtos/ProdutoNovo.jsx
@@ -1,40 +1,51 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useHistory, Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory, Link } from 'react-router-dom'
 
-import ProdutoForm from './ProdutoForm';
-import { insertProduto } from '../../store/produtos/actions';
-import { Container, Row, Col, Breadcrumb, BreadcrumbItem, Card, CardBody } from 'reactstrap';
+import ProdutoForm from './ProdutoForm'
+import { insertProduto } from '../../store/produtos/actions'
+import {
+  Container,
+  Row,
+  Col,
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+} from 'reactstrap'
+import ProdutoImageUpload from './ProdutoImageUpload'
+import { saveProductImage } from '../../store/produtos/actions'
 
 const ProdutoNovo = () => {
-    const dispatch = useDispatch()
-    const history = useHistory()
+  const dispatch = useDispatch()
+  const history = useHistory()
 
-    const handleSubmit = async e => {
-        await dispatch(insertProduto(e))
-        history.push("/produtos")
-    }
+  const handleSubmit = async (e) => {
+    await dispatch(insertProduto(e))
+    history.push('/produtos')
+  }
 
-    return <Container>
-        <Row>
-            <Col xl="12">
-                <Breadcrumb>
-                    <BreadcrumbItem tag={Link} to="/produtos">
-                        Produtos
-                    </BreadcrumbItem>
-                    <BreadcrumbItem>
-                        Alterar
-                    </BreadcrumbItem>
-                </Breadcrumb>
-            </Col>
-        </Row>
-        <Card>
-            <CardBody>
-                <ProdutoForm initialValues={{tags:[]}} onSubmit={handleSubmit} />
-            </CardBody>
-        </Card>
+  const handleOnProductImageSaved = async (formData) => {
+    const savedProductImage = await dispatch(saveProductImage(formData))
+    history.push(`/produtos/editar/${savedProductImage.data._id}`)
+  }
 
+  return (
+    <Container>
+      <Row>
+        <Col xl="12">
+          <Breadcrumb>
+            <BreadcrumbItem tag={Link} to="/produtos">
+              Produtos
+            </BreadcrumbItem>
+            <BreadcrumbItem>Novo Produto</BreadcrumbItem>
+          </Breadcrumb>
+        </Col>
+      </Row>
+
+      <ProdutoImageUpload onSave={handleOnProductImageSaved} />
+      <ProdutoForm initialValues={{ tags: [] }} onSubmit={handleSubmit} />
     </Container>
+  )
 }
 
 export default ProdutoNovo
--- src/screens/produtos/Produtos.jsx
+++ src/screens/produtos/Produtos.jsx
@@ -25,7 +25,7 @@ export default () => {
   const dispatch = useDispatch()
   const history = useHistory()
   const routeMatch = useRouteMatch()
-  const { data, currentPage, totalPages } = useSelector(
+  const { data, currentPage, totalPages, itemsPerPage } = useSelector(
     (state) => state.produtos.response,
   )
 
@@ -40,55 +40,114 @@ export default () => {
 
   React.useEffect(updateFilter, [])
 
-  const renderProdutos = () =>
-    data.map((produto) => (
-      <tr
-        className={produto.inactive ? 'bg-secondary' : ''}
-        key={`produto-${produto._id}`}
-      >
-        <th scope="row">{produto.name}</th>
-        <td>{produto.amount}</td>
-        <td>{produto.unit ? produto.unit.toUpperCase() : '--'}</td>
-        <td align="right">R$ {Number(produto.sellPrice).toFixed(2)}</td>
-        <td align="center">
-          <input
-            type="checkbox"
-            checked={produto.inactive}
-            onChange={async ({ target }) => {
-              await dispatch(changeInactive(produto._id, !produto.inactive))
-              updateFilter()
-            }}
-          />
-        </td>
-        <td className="d-flex justify-content-end">
-          <Button
-            disabled={produto.inactive}
-            className="mr-2"
-            outline
-            color="warning"
-            tag={Link}
-            to={`/produtos/editar/${produto._id}`}
-          >
-            <FontAwesomeIcon icon="pencil-alt" />
-          </Button>
+  const renderCardProducts = () =>
+    data.map((product) => (
+      <>
+        <Col sm="12" md="12" xl="12" className="mb-4">
+          <div className="card border-light shadow">
+            <div class="row">
+              <div class="col-md-2">
+                <img
+                  src={
+                    product.images.length > 0
+                      ? product.images[0]
+                      : 'http://via.placeholder.com/190x190'
+                  }
+                  width="190"
+                  alt={product.name}
+                />
+              </div>
+              <div class="col-md-8 pl-4">
+                <div className="card-body py-3">
+                  <div className="d-flex">
+                    <div>
+                      <h5 className="card-title">{product.name}</h5>
+                      <h6 className="card-subtitle text-muted">
+                        R$ {Number(product.sellPrice).toFixed(2)}{' '}
+                        {product.unit ? product.unit.toUpperCase() : ' '}
+                      </h6>
+                      <small className="font-weight-bold">{product.sku}</small>
+                    </div>
+                    <div className="ml-5">
+                      <p>
+                        <span class="d-block font-weight-bold">Quantidade</span>
+                        <span className="text-muted">{product.amount}</span>
+                      </p>
+                    </div>
+                    <div className="ml-5">
+                      <p>
+                        <span class="d-block font-weight-bold">
+                          Preço de Compra
+                        </span>
+                        <span className="text-muted">
+                          {product.buyPrice
+                            ? `R$ ${Number(product.buyPrice).toFixed(2)}`
+                            : '--'}
+                        </span>
+                      </p>
+                    </div>
+                    <div className="ml-5">
+                      <div className="form-group">
+                        <span class="d-block font-weight-bold">Inativo</span>
+                        <select
+                          className="form-control"
+                          value={product.inactive}
+                          onChange={async () => {
+                            await dispatch(
+                              changeInactive(product._id, !product.inactive),
+                            )
+                            updateFilter()
+                          }}
+                        >
+                          <option value={true}>Sim</option>
+                          <option value={false}>Não</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <p className="card-text my-2">{product.description}</p>
+                  {product.tags.map((tag) => (
+                    <span class="badge badge-pill badge-primary mr-2">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div className="card-body">
+                  <Button
+                    disabled={product.inactive}
+                    className="mr-2"
+                    outline
+                    color="warning"
+                    tag={Link}
+                    to={`/produtos/editar/${product._id}`}
+                  >
+                    <FontAwesomeIcon icon="pencil-alt" />
+                  </Button>
 
-          <Button
-            outline
-            disabled={produto.inactive}
-            color="danger"
-            tag={Link}
-            onClick={async () => {
-              if (!window.confirm('Tem certeza de que deseja deletar?')) {
-                return
-              }
-              await dispatch(deleteProduto(produto._id))
-              updateFilter()
-            }}
-          >
-            <FontAwesomeIcon icon="trash" />
-          </Button>
-        </td>
-      </tr>
+                  <Button
+                    outline
+                    color="danger"
+                    tag={Link}
+                    onClick={async () => {
+                      if (
+                        !window.confirm('Tem certeza de que deseja deletar?')
+                      ) {
+                        return
+                      }
+                      await dispatch(deleteProduto(product._id))
+                      updateFilter()
+                    }}
+                  >
+                    <FontAwesomeIcon icon="trash" />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </>
     ))
 
   const paginate = (page) => {
@@ -118,75 +177,87 @@ export default () => {
     ))
   }
 
-  return <Container>
-    <Row className="mb-2">
-      <Col xl="12">
-        <Breadcrumb>
-          <BreadcrumbItem>Produtos</BreadcrumbItem>
-        </Breadcrumb>
-      </Col>
-      <Col xl="12">
-        <div className="d-flex align-self-center justify-content-end">
-          <Button outline color="success" tag={Link} to="/produtos/novo">
-            Novo produto <FontAwesomeIcon icon="plus" />
-          </Button>
-        </div>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <Table striped bordered size="sm">
-          <thead className="thead-light">
-            <tr>
-              <th>Nome</th>
-              <th>Quantidade</th>
-              <th>Unidade</th>
-              <th className="text-right">$ Venda</th>
-              <th className="text-center">Inativo</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>{renderProdutos()}</tbody>
-        </Table>
-      </Col>
-    </Row>
-    <Row>
-      <Col className="d-flex justify-content-center">
-        <Pagination>
-          <PaginationItem>
-            <PaginationLink
-              disabled={currentPage <= 0}
-              first
-              onClick={() => paginate(0)}
-              to="#"
-            />
-          </PaginationItem>
-          <PaginationItem disabled={currentPage <= 0}>
-            <PaginationLink
-              previous
-              onClick={() => paginate(currentPage - 1)}
-              to="#"
-            />
-          </PaginationItem>
-          {renderPaginationItems()}
-          <PaginationItem>
-            <PaginationLink
-              disabled={currentPage >= totalPages - 1}
-              next
-              onClick={() => paginate(currentPage + 1)}
-              to="#"
-            />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink
-              disabled={currentPage >= totalPages - 1}
-              last
-              onClick={() => paginate(totalPages - 1)}
-              to="#"
-            />
-          </PaginationItem>
-        </Pagination>
-      </Col>
-    </Row>
-  </Container>
+  return (
+    <Container>
+      <Row className="mb-2">
+        <Col xl="12">
+          <Breadcrumb>
+            <BreadcrumbItem>Produtos</BreadcrumbItem>
+          </Breadcrumb>
+        </Col>
+        <Col xl="12">
+          <div className="d-flex mb-3">
+            <div className="form-group mb-0 flex-grow-1 mr-2">
+              <input
+                placeholder="Busque pelo nome do produto ou sku..."
+                type="text"
+                name="search"
+                className="form-control"
+              />
+            </div>
+            <Button
+              className="mr-2"
+              outline
+              color="info"
+              tag={Link}
+              to="/produtos/novo"
+            >
+              Buscar <FontAwesomeIcon icon="search" />
+            </Button>
+
+            <Button outline color="success" tag={Link} to="/produtos/novo">
+              Novo produto <FontAwesomeIcon icon="plus" />
+            </Button>
+          </div>
+          <div>
+            <p>
+              <span className="font-weight-bold">Total de produtos: </span>
+              <span class="text-muted font-italic">
+                {itemsPerPage * totalPages}
+              </span>
+            </p>
+          </div>
+        </Col>
+      </Row>
+      <Row>{renderCardProducts()}</Row>
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <Pagination>
+            <PaginationItem>
+              <PaginationLink
+                disabled={currentPage <= 0}
+                first
+                onClick={() => paginate(0)}
+                to="#"
+              />
+            </PaginationItem>
+            <PaginationItem disabled={currentPage <= 0}>
+              <PaginationLink
+                previous
+                onClick={() => paginate(currentPage - 1)}
+                to="#"
+              />
+            </PaginationItem>
+            {renderPaginationItems()}
+            <PaginationItem>
+              <PaginationLink
+                disabled={currentPage >= totalPages - 1}
+                next
+                onClick={() => paginate(currentPage + 1)}
+                to="#"
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink
+                disabled={currentPage >= totalPages - 1}
+                last
+                onClick={() => paginate(totalPages - 1)}
+                to="#"
+              />
+            </PaginationItem>
+          </Pagination>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
